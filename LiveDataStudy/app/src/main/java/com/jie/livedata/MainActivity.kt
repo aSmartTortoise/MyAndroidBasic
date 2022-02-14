fda4c758aa1dd15385c84f9e36bce217973b1d6a
aSmartTortoise <wyjonneteasy@163.com>
2022-02-14 21:38:53 +0800

    Android基础知识之LiveData：MediatorLiveData合并多个LiveData数据源

--- LiveDataStudy/app/src/main/java/com/jie/livedata/MainActivity.kt
+++ LiveDataStudy/app/src/main/java/com/jie/livedata/MainActivity.kt
@@ -3,13 +3,12 @@ package com.jie.livedata
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.arch.core.util.Function
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.*
 import kotlin.Function as Function1001
 
 /**
@@ -22,8 +21,17 @@ import kotlin.Function as Function1001
  *  取消订阅这种观察被观察关系。
  *  MutableLiveData是LiveData的实现类。有postValue和setValue方法，setValue方法只能在主线程调用。
  *  postValue方法没有限制。
+ *  1 更改LiveData中的数据
  *  如果想在LiveData对象分发给观察者之前，修改其中的数据，可以使用Transformations.map()方法和
  *  Transformations.switchMap().
+ *  2 合并多个LiveData数据源
+ *      MediatorLiveData继承MutableLiveData，MediatorLiveData可以合并多个LiveData数据源，
+ *  当其中任意一个LiveData数据源的数据发生变化，都会通知给处于ACTIVE状态的LifecycleOwner。
+ *
+ *  4 LiveData的onActive和onInactive方法
+ *  当LiveData关联的处于ACTIVE的观察者数量由0变为1的时候会回调onActive方法；当LiveData关联的处于ACTIVE
+ *  状态的观察者数量由1变为0的时候会回调onInactive方法。
+ *
  *
  */
 class MainActivity : AppCompatActivity() {
@@ -82,5 +90,20 @@ class MainActivity : AppCompatActivity() {
         mLiveData3.postValue(false)
         mLiveData1.postValue("Android Jetpack architecture component: LiveData 1")
         mLiveData2.postValue("Android Jetpack architecture component: LiveData 2")
+
+        findViewById<Button>(R.id.btn_mediator).setOnClickListener { v -> mediatorStudy() }
+    }
+
+    private fun mediatorStudy() {
+        val liveData1 = MutableLiveData<String>()
+        val liveData2 = MutableLiveData<String>()
+        val mediatorLiveData = MediatorLiveData<String>()
+        mediatorLiveData.addSource(liveData1) {t -> Log.d(TAG, "mediatorStudy: wyj t:$t")}
+        mediatorLiveData.addSource(liveData2) {s -> Log.d(TAG, "mediatorStudy: wyj s:$s")}
+        mediatorLiveData.observe(this) {result -> Log.d(TAG, "mediatorStudy: wyj result:$result")}
+        liveData1.postValue("Mediator: Android Jetpack architecture component:")
+        Handler().postDelayed({
+                              liveData2.postValue("LiveData")
+        }, 200L)
     }
 }
