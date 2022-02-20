3741b26ed2ed3952273cbd3f5dc03080bf71176e
aSmartTortoise <wyjonneteasy@163.com>
2022-02-20 21:01:55 +0800

    Android基础知识之ViewModel：引入fragment-ktx库 Fragment中扩展方法activityViewModels获取ViewModel实例。

--- ViewModelStudy/app/src/main/java/com/jie/viewmodel/fragment/FragmentB.kt
+++ ViewModelStudy/app/src/main/java/com/jie/viewmodel/fragment/FragmentB.kt
@@ -7,17 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import com.jie.viewmodel.R
 import com.jie.viewmodel.ShareDataModel
 
 class FragmentB : Fragment() {
-    lateinit var mViewModel: ShareDataModel
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        mViewModel = ViewModelProvider(activity!!).get(ShareDataModel::class.java)
-    }
+    val mViewModel: ShareDataModel by activityViewModels<ShareDataModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
