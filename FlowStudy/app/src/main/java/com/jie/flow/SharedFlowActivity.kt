a6daf82d6fbecd6a087a0eb3e98d8e8a211fe1b7
aSmartTortoise <wyjonneteasy@163.com>
2022-05-09 21:03:48 +0800

    Android基础知识之SharedFlow：replayCache属性

--- FlowStudy/app/src/main/java/com/jie/flow/SharedFlowActivity.kt
+++ FlowStudy/app/src/main/java/com/jie/flow/SharedFlowActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.jie.flow.viewmodel.TestFlowViewModel
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
@@ -20,21 +21,40 @@ class SharedFlowActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_flow)
         lifecycleScope.launch {
-            launch {
-                viewModel.mShareFlow.collect {
-                    Log.d(TAG, "onCreate: wyj shared first launch element:$it")
-                }
-            }
-
-            launch {
-                delay(3000L)
-                viewModel.mShareFlow.collect {
-                    Log.d(TAG, "onCreate: wyj shared second launch element:$it")
-                }
-            }
+//            sharedFlowImpReplayStudy()
+            sharedFlowReplayCacheStudy()
 
         }
         viewModel.downloadBySharedFlow()
+    }
+
+    /**
+     *  当有数据变化的时候，SharedFlow会把新的数据存到buffer中。当有新的订阅者时，优先从buffer中获取值，
+     *  获取的replayCache只是指定replay大小的片段
+     */
+    private suspend fun sharedFlowReplayCacheStudy() {
+        var index = 0
+        viewModel.mShareFlow.collect {
+            Log.d(
+                TAG,
+                "onCreate: wyj 第${index++}次变化，element:$it, replayCache:${viewModel.mShareFlow.replayCache}"
+            )
+        }
+    }
+
+    private fun CoroutineScope.sharedFlowImpReplayStudy() {
+        launch {
+            viewModel.mShareFlow.collect {
+                Log.d(TAG, "onCreate: wyj shared first launch element:$it")
+            }
+        }
+
+        launch {
+            delay(3000L)
+            viewModel.mShareFlow.collect {
+                Log.d(TAG, "onCreate: wyj shared second launch element:$it")
+            }
+        }
     }
 
     override fun onDestroy() {
