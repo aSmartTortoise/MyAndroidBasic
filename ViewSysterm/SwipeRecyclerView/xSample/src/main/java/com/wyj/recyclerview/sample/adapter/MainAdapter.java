569720a6c157b9c653714bfe822f3eefbe6fa396
王永杰 <xiayuexingkong@gmail.com>
2025-08-12 15:12:30 +0800

    添加SwipeRecyclerView project

---
+++ ViewSysterm/SwipeRecyclerView/xSample/src/main/java/com/wyj/recyclerview/sample/adapter/MainAdapter.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2016 Jie Wong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wyj.recyclerview.sample.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wyj.recyclerview.sample.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created by YOLANDA on 2016/7/22.
+ */
+public class MainAdapter extends BaseAdapter<MainAdapter.ViewHolder> {
+
+    private List<String> mDataList;
+
+    public MainAdapter(Context context) {
+        super(context);
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void notifyDataSetChanged(List<String> dataList) {
+        this.mDataList = dataList;
+        super.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList == null ? 0 : mDataList.size();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(getInflater().inflate(R.layout.item_menu_main, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setData(mDataList.get(position));
+    }
+
+    public List<String> getDataList() {
+        return mDataList;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+        }
+
+        public void setData(String title) {
+            this.tvTitle.setText(title);
+        }
+    }
+
+}
